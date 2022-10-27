6746a1120abafdcc3af1d486a66182eb6ef37053
Zach <tuj54380@temple.edu>
2022-10-27 14:42:18 -0400

    got a webpage to display and volly to work

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -1,25 +1,54 @@
 package edu.temple.basicbrowser
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
+
 
     lateinit var urlEditText: EditText
     lateinit var goButton: ImageButton
     lateinit var webView: WebView
+    lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+       // webView.settings.javaScriptEnabled = true
         urlEditText = findViewById(R.id.urlEditText)
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+
+        // using Volly
+        requestQueue = Volley.newRequestQueue(this)
+        // this will load the url of the website
+      //  webView.loadUrl("https://temple.edu")
+
+
+        requestQueue.add(
+            StringRequest(Request.Method.GET, "https://www.temple.edu", {
+                webView.loadDataWithBaseURL("", it, "text/html", "utf-8", null)
+            }, {})
+        )
 
         // Allow your browser to intercept hyperlink clicks
         webView.webViewClient = object: WebViewClient() {
@@ -27,6 +56,5 @@ class MainActivity : AppCompatActivity() {
                 super.onPageFinished(view, url)
             }
         }
-
     }
 }
